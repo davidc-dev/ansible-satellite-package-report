686351ca0d36b25650055945d06c5074a769a6bb
davidc-dev <58272339+davidc-dev@users.noreply.github.com>
2023-02-22 19:27:03 -0600

    remove pandas req

--- files/2_packages_errata_id.py
+++ files/2_packages_errata_id.py
@@ -1,10 +1,11 @@
 import json
-import pandas as pd
 
 with open("combined.json", "r") as f:
     data = json.load(f)
 
 rows = []
+packages = set()
+
 for errata_id, errata_data in data.items():
     for package in errata_data['packages']:
         if package.endswith('.i686'):
@@ -13,12 +14,14 @@ for errata_id, errata_data in data.items():
             package = package[:-7] # remove the last 7 characters (.x86_64)
         elif package.endswith('.noarch'):
             package = package[:-7] # remove the last 7 characters (.noarch)
-        if package not in [row['package_name'] for row in rows]:
+        if package not in packages:
             rows.append({
                 'package_name': package,
                 'errata_id': errata_id,
             })
+            packages.add(package)
 
-df = pd.DataFrame(rows)
-df.to_csv('packages_errata.csv', index=False)
-
+with open('packages_errata.csv', 'w') as f:
+    f.write('package_name,errata_id\n')
+    for row in rows:
+        f.write('{},{}\n'.format(row['package_name'], row['errata_id']))
